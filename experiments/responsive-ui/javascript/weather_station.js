5b22f545f5c46ab8dd82caf3212ad9b40a3db9ec
Ketil Erichsen <Ketil.Erichsen@nordicsemi.no>
2016-02-11 10:43:07 +0100

    test of reading char

--- experiments/responsive-ui/javascript/weather_station.js
+++ experiments/responsive-ui/javascript/weather_station.js
@@ -207,6 +207,10 @@ function getTemperature() {
     value = value.buffer ? value : new DataView(value);
     let batteryLevel = value.getUint8(0);
     log('> Temperature is ' + batteryLevel + 'C');
+    let batteryLevel = value.getUint16(1, true);
+    log('> Temperature is ' + batteryLevel + 'C');
+    let batteryLevel = value.getUint8(0, true);
+    log('> Temperature is ' + batteryLevel + 'C');
   })
   .catch(error => {
     log('Argh! ' + error);
