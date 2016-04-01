18987cfe1d840523b8879fb819540379dd57e96f
Ketil Erichsen <Ketil.Erichsen@nordicsemi.no>
2016-04-01 15:31:16 +0200

    fixed typo

--- experiments/notifications/notifications.js
+++ experiments/notifications/notifications.js
@@ -46,7 +46,7 @@ function connect() {
   })
   .then(() => bleService.getCharacteristic(button4characteristicUUID))
   .then( characteristic => {
-    log('Got button3characteristic');
+    log('Got button4characteristic');
     button4char = characteristic;
     return button4char.startNotifications();
   })
