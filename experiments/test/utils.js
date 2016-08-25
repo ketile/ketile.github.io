5be3c0aa663e4b38fd806d70baf817cacf9ab90d
Ketil Erichsen <Ketil.Erichsen@nordicsemi.no>
2016-08-25 16:17:27 +0200

    added logProperties()

--- experiments/test/utils.js
+++ experiments/test/utils.js
@@ -5,3 +5,21 @@ function log(message){
 //    var content = document.createTextNode(message);
 //    logs.appendChild(content);
 }
+
+function logProperties(characteristic){
+    log('> UUID:                 ' + characteristic.uuid);
+    log('> Broadcast:            ' + characteristic.properties.broadcast);
+    log('> Read:                 ' + characteristic.properties.read);
+    log('> Write w/o response:   ' +
+      characteristic.properties.writeWithoutResponse);
+    log('> Write:                ' + characteristic.properties.write);
+    log('> Notify:               ' + characteristic.properties.notify);
+    log('> Indicate:             ' + characteristic.properties.indicate);
+    log('> Signed Write:         ' +
+      characteristic.properties.authenticatedSignedWrites);
+    log('> Queued Write:         ' + characteristic.properties.reliableWrite);
+    log('> Writable Auxiliaries: ' +
+      characteristic.properties.writableAuxiliaries);
+    log('> Value (cached):       ' + characteristic.value);
+    log(' ');
+}
